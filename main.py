3a0477992bd01e350a7d922c24c60257a2337274
Jack Bonilla <jbonil16@calpoly.edu>
2026-04-09 10:49:40 -0700

    First Commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Create a welcome message.
 # Input: a name as a string
 # Result: a string
-def welcome_message(jbonil16@calpoly.edu:str) -> str:
+def welcome_message( s:str) -> str:
    message = "Hello, " + name + "."
 
    return message
